f41de4b3276bc9c30b999c5991b4da0bc7d6ea4a
Steve Samson <stevee.samson@gmail.com>
2016-10-05 16:58:37 +0100

    __.express added so it can be used as express view engine

--- dist/stud.js
+++ dist/stud.js
@@ -18,7 +18,7 @@
             if (!isStud(this)) return new stud();
             this.cache = {};
         },
-        isString = function(o){
+        isString = function (o) {
             return typeof o === 'string';
         };
     stud.fn = stud.prototype;
@@ -27,14 +27,14 @@
     };
     stud.fn.render = function (name, data, cb) {
         var str = this.cache[name](data);
-        if(cb) cb(str);
+        if (cb) cb(str);
         else return str;
     };
     stud.fn.isRegistered = function (name) {
         return !!this.cache[name];
     };
     stud.fn.register = function (name, fn) {
-        if(!name || !fn || !(typeof fn === 'function')){
+        if (!name || !fn || !(typeof fn === 'function')) {
             console.error("Cannot register template...");
             return;
         }
@@ -43,7 +43,7 @@
 
     stud.fn.compile = function (tmplString, tmplName, cb) {
 
-        var compileNow = function(template, name){
+        var compileNow = function (template, name) {
 
             var re = /([^{]*)?(\{(\w+)\})([^{]*)?/ig, sb = require('strbuilder')();
             template.replace(
@@ -69,11 +69,45 @@
             return "(function(c){var b = c.buffer();c.register(\"" + name + "\",function(x){" + sb.toString() + "; return b.toString();});}(stud));";
         };
 
-        if(isString(tmplString) && isString(tmplName)){
-            if(cb){
+        if (isString(tmplString) && isString(tmplName)) {
+            if (cb) {
                 cb(compileNow(tmplString, tmplName));
-            }else return compileNow(tmplString, tmplName);
+            } else return compileNow(tmplString, tmplName);
         }
+
+    };
+    stud.fn.__express = function (filePath, options, cb) { // define the template engine
+
+        require('fs').readFile(filePath, function (err, template) {
+
+            if (err) return cb(new Error(err));
+
+            template = template.toString();
+            (function () {
+
+                var re = /([^{]*)?(\{(\w+)\})([^{]*)?/ig,
+                    sb = require('strbuilder')();
+
+                template.replace(
+                    re,
+                    function ($0, $1, $2, $3, $4) {
+                        if ($1) {
+                            sb.append($1);
+                        }
+                        if ($3) {
+                            sb.append(options[$3]);
+                        }
+
+                        if ($4) {
+                            sb.append($4);
+                        }
+
+                        return;
+                    }
+                );
+                cb && cb(false, sb.toString());
+            })();
+        });
 
     };
 
--- tests/stud_tests.js
+++ tests/stud_tests.js
@@ -1,15 +1,15 @@
 var expect = require('chai').expect,
     should = require('chai').should(),
-    data = {name:'steve', age:23, location:'US'},
+    data = {name: 'steve', age: 23, location: 'US'},
     tmpl = "<td class='{name}'> {name} - {age} some of them are useful at location {location} as specified in the docs.</td>",
     shouldRender = "<td class='steve'> steve - 23 some of them are useful at location US as specified in the docs.</td>",
     stud = null, compiledTmpl = null;
 
-describe("#Stud Test Cases", function(){
+describe("#Stud Test Cases", function () {
 
-    before(function(done){
-         stud = require('../dist/stud');
-         done();
+    before(function (done) {
+        stud = require('../dist/stud');
+        done();
         //
         //
         ///*The template will be registered with the name 'template_reg_name' */
@@ -25,54 +25,73 @@ describe("#Stud Test Cases", function(){
         //});
     });
 
-    describe("#Ensure Stud was loaded",function(){
+    describe("#Ensure Stud was loaded", function () {
 
-        it("Expect stud to be defined", function(){
-           expect(stud).to.be.defined;
+        it("Expect stud to be defined", function () {
+            expect(stud).to.be.defined;
         });
 
-        it("Expect stud.compile to be defined", function(){
+        it("Expect stud.compile to be defined", function () {
             expect(stud.compile).to.be.defined;
         });
 
-        it("Expect stud.render to be defined", function(){
+        it("Expect stud.render to be defined", function () {
             expect(stud.render).to.be.defined;
         });
 
     });
 
-    describe("#Compile with Stud",function(){
+    describe("#Compile with Stud", function () {
 
-        it("Expect compiledTmpl to be null", function(){
+        it("Expect compiledTmpl to be null", function () {
             expect(compiledTmpl).to.be.equal(null);
         });
 
-        it("Expect compiledTmpl not to be null", function(){
-            compiledTmpl = stud.compile(tmpl,"test_template.stud");
+        it("Expect compiledTmpl not to be null", function () {
+            compiledTmpl = stud.compile(tmpl, "test_template.stud");
             expect(compiledTmpl).not.to.be.equal(null);
         });
 
     });
 
-    describe("#Registering with Stud",function(){
+    describe("#Registering with Stud", function () {
 
-        it("Expect stud.isRegistered('test_template.stud') to be false", function(){
+        it("Expect stud.isRegistered('test_template.stud') to be false", function () {
             expect(stud.isRegistered('test_template.stud')).to.be.equal(false);
         });
 
-        it("Expect stud.isRegistered('test_template.stud') to be true", function(){
+        it("Expect stud.isRegistered('test_template.stud') to be true", function () {
             eval(compiledTmpl);
             expect(stud.isRegistered('test_template.stud')).to.be.equal(true);
         });
 
     });
 
-    describe("#Rendering with Stud",function(){
+    describe("#Rendering with Stud", function () {
 
-        it("Expect res to be equal to shouldRender", function(done){
-            stud.render('test_template.stud',data, function(res){
+        it("Expect res to be equal to shouldRender", function (done) {
+            stud.render('test_template.stud', data, function (res) {
                 expect(res).to.be.equal(shouldRender);
                 done();
+            });
+
+        });
+
+    });
+
+    describe("#Render with __.express as view engine", function () {
+
+        it("Expect res to be equal to shouldRender", function (done) {
+            stud.__express(__dirname + '/user.stud', data, function (e, res) {
+                if (e) {
+                    console.log(e.toString());
+
+                    //done();
+                }
+                expect(res).to.be.equal(shouldRender);
+                done();
+
+
             });
 
         });
